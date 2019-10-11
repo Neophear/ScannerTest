ebb6fb5fe0ef5177a23e571ad4e2ab2e8f2ba0fa
Neophear <stiiggade@gmail.com>
2019-10-11 10:41:23 +0200

    Initial commit

---
+++ ScannerTest/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScannerTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            textBox1.Text += Environment.NewLine + e.KeyData;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            textBox1.Text += Environment.NewLine + e.KeyCode;
+        }
+    }
+}
